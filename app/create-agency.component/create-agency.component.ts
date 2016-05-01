6ad4ee9b353b4fda502ae192467c4adf834e942c
karthikchintala64 <kart.963@gmail.com>
2016-05-01 18:45:42 -0400

    create agency

---
+++ app/create-agency.component/create-agency.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector:'create-agency',
+    styleUrls:['app/create-agency.component/create-agency.component.css'],
+    templateUrl:'app/create-agency.component/create-agency.component.html'
+})
+
+export class CreateAgencyComponent{
+    
+}
